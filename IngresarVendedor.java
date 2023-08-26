4c1abeea493ef3cef18ae621631e072782f43c90
EdwinTipan <143285653+EdwinTipan@users.noreply.github.com>
2023-08-26 13:07:48 -0500

    Update IngresarVendedor.java

--- IngresarVendedor.java
+++ IngresarVendedor.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
 
 public class IngresarVendedor {
-	public void ingreso(int id, String nombreIn, String apellidoIn, String usuIN,S tring conIN) {
+	public void ingreso(int id, String nombreIn, String apellidoIn, String usuIN, String conIN) {
 		// TODO Auto-generated method stub
 		MySQLConnection conex = new MySQLConnection();
 		
